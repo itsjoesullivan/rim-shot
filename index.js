05d5b12530880b593c3d1f1e64caca89e10797fd
Joe Sullivan <itsjoesullivan@gmail.com>
2016-05-17 10:25:24 -0400

    Move shared graph to constructor

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 var makeDistortionCurve = require('make-distortion-curve');
+var curve = makeDistortionCurve(1024);
+
 
 // partially informed by the rather odd http://www.kvraudio.com/forum/viewtopic.php?t=383536
 module.exports = function(context) {
+
+  var distortion = context.createWaveShaper();
+  distortion.curve = curve;
+
+  var highpass = context.createBiquadFilter();
+  highpass.type = "highpass";
+  highpass.frequency.value = 700;
+
+  distortion.connect(highpass);
 
   return function() {
     var duration = 0.05;
     var oscs = [
       context.createOscillator(),
       context.createOscillator(),
-      //context.createOscillator()
       ];
     oscs.forEach(function(osc) {
       osc.type = "triangle";
     });
-    var highpass = context.createBiquadFilter();
-    highpass.type = "highpass";
-    highpass.frequency.value = 700;
     oscs[0].frequency.value = 500;
     oscs[1].frequency.value = 1720;
-    var distortion = context.createWaveShaper();
-    distortion.curve = makeDistortionCurve(1024);
 
     var gain = context.createGain();
+
     oscs.forEach(function(osc) {
       osc.connect(distortion);
     });
-    distortion.connect(highpass);
     highpass.connect(gain);
     gain.start = function(when) {
       oscs.forEach(function(osc) {
